6910a168d59897f359188853c40c95c5227029c1
Elizabeth <elizabethallen36@gmail.com>
2020-12-14 16:52:31 -0800

    tried to get some js animations to work, and to get a scroll reveal, but it just keeps hiding my navbar.

--- js/styles.js
+++ js/styles.js
@@ -1,3 +1,8 @@
+// scroll revela start 
+// ScrollReveal().reveal('.htmlclass',{ delay: 400 })
+// ScrollReveal().reveal('.navbar');
+// scroll reveal end
+// start navbar
 $(document).ready(function () {
 
     $('.first-button').on('click', function () {
@@ -13,3 +18,33 @@ $(document).ready(function () {
       $('.animated-icon3').toggleClass('open');
     });
   });
+// end navbar
+
+// start languages
+// declaring variables
+
+// const card = document.querySelector(".card");
+// const title = document.querySelector(".title");
+// const header = document.querySelector(".header img");
+// const description = document.querySelector(".info h3");
+
+// // Animate In
+
+// card.addEventListener("mouseenter", (e) => {
+//   card.style.transition = "none";
+//   title.style.transform = "translateZ(3px)";
+//   header.style.transform = "translateZ(3px) rotateZ(0deg)";
+//   description.style.transform = "translateZ(3px)";
+// });
+
+// // Animate Out
+
+// card.addEventListener("mouseleave", (e) => {
+//   card.style.transition = "all 0.5s ease";
+//   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+//   title.style.transform = "translateZ(0px)";
+//   header.style.transform = "translateZ(0px) rotateZ(0deg)";
+//   description.style.transform = "translateZ(0px)";
+// });
+// end languages
+
